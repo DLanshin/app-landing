8f930fe961eb5d05bce17e98c7ce50c9069b84b5
DLanshin <DanilLanshin@gmail.com>
2022-07-02 18:11:14 +0300

    Верстка главной страницы *шапка *подвал *слайдер на главной *коллекция на главной *блок с баннером

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
- $(function () {
-
+$(function () {
+    $(".header__btn, .rightside-menu__close").on("click", function () {
+        $(".rightside-menu").toggleClass("rightside-menu-close")
+    });
+    $(".top__slider").slick({
+        dots: true,
+        arrows: false,
+        autoPlay: true,
+    });
 })
