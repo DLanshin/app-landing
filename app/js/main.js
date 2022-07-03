5724e4f22b9b5c3499f53419065159ff44e4ac1b
DLanshin <DanilLanshin@gmail.com>
2022-07-03 21:10:44 +0300

    Доработана все страницы

--- app/js/main.js
+++ app/js/main.js
@@ -15,9 +15,50 @@ $(function () {
         slidesToShow: 5,
         slidesToScroll: 5
     });
-    var mixer = mixitup(".gallery__inner",{
-        load:{
-            filter: ".bedroom"
-        }
-    });
+    debugger
+    $(".collection").each( function(i, item){
+
+        var count = parseInt(item.getAttribute("data-slides"));
+        $(item).slick({
+            dots: true,
+            arrows: false,
+            autoPlay: false,
+            infinite: true,
+            slidesToShow: count,
+            slidesToScroll: count,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
+    })
+    if($(".gallery__inner").length){
+        var mixer = mixitup(".gallery__inner",{
+            load:{
+                filter: ".bedroom"
+            }
+        });
+    }
 })
