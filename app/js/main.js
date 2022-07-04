4cade93a71c253dce93bbb3f4079aae92a92c22d
DLanshin <DanilLanshin@gmail.com>
2022-07-04 12:22:55 +0300

    Доделал адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $(".header__btn, .rightside-menu__close").on("click", function () {
         $(".rightside-menu").toggleClass("rightside-menu-close")
     });
@@ -13,11 +14,36 @@ $(function () {
         autoPlay: false,
         infinite: true,
         slidesToShow: 5,
-        slidesToScroll: 5
-    });
-    debugger
-    $(".collection").each( function(i, item){
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
 
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: false
+                }
+            }
+        ]
+    });
+    $(".collection").each( function(i, item){
         var count = parseInt(item.getAttribute("data-slides"));
         $(item).slick({
             dots: true,
@@ -40,14 +66,17 @@ $(function () {
                     breakpoint: 600,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToScroll: 2,
+                        dots: false
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
                         slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToScroll: 1,
+                        dots: false
+
                     }
                 }
             ]
